97c10bfb8e1cb9b2d0d0d388496ffea145dbae68
Nisha <nkolivia1234@gmail.com>
2023-06-12 15:01:54 +0530

    Refactor:group similar tasks in tictactoe test file.

--- src/testcases/TicTacToe.spec.js
+++ src/testcases/TicTacToe.spec.js
@@ -1,20 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import TicTacToe from '../components/TicTacToe';
 
-test('should load TicTacToe', () => {
-  render(<TicTacToe />);
-  const appComponent = screen.getByTestId('parent');
-  expect(appComponent).toBeInTheDocument();
+describe("The TicTacToe app works fine when", () => {
+  beforeEach(() => {
+    render(<TicTacToe />);
+  });
+
+  it("should load TicTacToe", () => {
+    const appComponent = screen.getByTestId('parent');
+    expect(appComponent).toBeInTheDocument();
+  });
+
+  it("should have the header loaded", () => {
+    const headerTitle = screen.getByTestId('headerTitle');
+    expect(headerTitle).toBeInTheDocument();
+  }); 
+
+  it("should have the proper header name", () => {
+    const headerTitleText = screen.getByTestId('headerTitle');
+    expect(headerTitleText).toHaveTextContent('Tic Tac Toe Game');
+  });
+
 });
 
-it("should have the header loaded", () => {
-  render(<TicTacToe />);
-  const headerTitle = screen.getByTestId('headerTitle');
-  expect(headerTitle).toBeInTheDocument();
-}); 
-
-it("should have the proper header name", () => {
-  render(<TicTacToe />);
-  const headerTitleText = screen.getByTestId('headerTitle');
-  expect(headerTitleText).toHaveTextContent('Tic Tac Toe Game');
-});
+ 
