9b89557bf93737563f43c630ab8e3eb0da5d942b
Nisha <nkolivia1234@gmail.com>
2023-06-12 18:50:50 +0530

    Refactor:group similar tasks in tile testcase file.

--- src/testcases/Tile.spec.js
+++ src/testcases/Tile.spec.js
@@ -1,14 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import Tile from "../components/Tile";
 
-it("should have one tile", () => {
-  render(<Tile value='tileText'/>);
-  const tile = screen.getByTestId("tileText");
-  expect(tile).toBeInTheDocument();
-});
+describe("The Fizzbuzz component works fine when", () => {
+  beforeEach(() => {
+      render(<Tile value='tileText'/>);
+  });
 
-it("should have the proper text of tile", () => {
-    render(<Tile value="tileText" />);
-    const tileText = screen.getByTestId("tileText");
-    expect(tileText).toHaveTextContent("tileText");
+    it("should have one tile", () => {
+      const tile = screen.getByTestId("tileText");
+      expect(tile).toBeInTheDocument();
+    });
+    
+    it("should have the proper text of tile", () => {
+        const tileText = screen.getByTestId("tileText");
+        expect(tileText).toHaveTextContent("tileText");
+    });
 });
