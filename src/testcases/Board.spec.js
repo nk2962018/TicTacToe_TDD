8a06d334942b7270d3e8c489682016dac3a2165f
Nisha <nkolivia1234@gmail.com>
2023-06-13 13:49:12 +0530

    Refactor:group similar tasks in board testcase file.

--- src/testcases/Board.spec.js
+++ src/testcases/Board.spec.js
@@ -3,23 +3,28 @@ import Board from '../components/Board';
 import Constants from '../components/constants/Constants';
 import Tile from '../components/Tile';
 
-it('should render board component' , () => {
-    render(<Board/>);
-    const boardComponent = screen.getByTestId('board');
-    expect(boardComponent).toBeInTheDocument();
-})
+describe('Board works fine when', () => {
+    beforeEach(() => {
+        render(<Board/>)
+    });
+    
+    it('should render board component' , () => {
+        const boardComponent = screen.getByTestId('board');
+        expect(boardComponent).toBeInTheDocument();
+    });
 
-it('should render 3x3 tiles' , () => {
-    render(<Board/>);
-    render(<Tile value='tiles'/>);
-    render(<Tile value='tiles'/>);
-    render(<Tile value='tiles'/>);
-    render(<Tile value='tiles'/>);
-    render(<Tile value='tiles'/>);
-    render(<Tile value='tiles'/>);
-    render(<Tile value='tiles'/>);
-    render(<Tile value='tiles'/>);
-    render(<Tile value='tiles'/>);
-    const tiles = screen.getAllByTestId('tiles');
-    expect(tiles).toHaveLength(Constants.NO_OF_TILES_IN_THE_BOARD);
-})
+    it('should render 3x3 tiles' , () => {
+        render(<Tile value='tiles'/>);
+        render(<Tile value='tiles'/>);
+        render(<Tile value='tiles'/>);
+        render(<Tile value='tiles'/>);
+        render(<Tile value='tiles'/>);
+        render(<Tile value='tiles'/>);
+        render(<Tile value='tiles'/>);
+        render(<Tile value='tiles'/>);
+        render(<Tile value='tiles'/>);
+        const tiles = screen.getAllByTestId('tiles');
+        expect(tiles).toHaveLength(Constants.NO_OF_TILES_IN_THE_BOARD);
+    });
+
+});
